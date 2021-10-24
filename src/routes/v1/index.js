a00bfa68f1d70f52741686ce7ad1cf18f3831439
crisog <ortega.cpp@gmail.com>
2021-10-24 16:51:20 -0400

    feat; enable basic auth endpoints

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const authRoute = require('./auth.route');
+const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const docsRoute = require('./docs.route');
 const config = require('../../config/config');
@@ -7,10 +7,10 @@ const config = require('../../config/config');
 const router = express.Router();
 
 const defaultRoutes = [
-  // {
-  //   path: '/auth',
-  //   route: authRoute,
-  // },
+  {
+    path: '/auth',
+    route: authRoute,
+  },
   {
     path: '/users',
     route: userRoute,
